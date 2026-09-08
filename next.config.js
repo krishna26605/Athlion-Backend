f4b94c0238c4146b6bfccc8910e2de320fffe7fa
krishnanikamdelxn <krishnanikam.delxn@gmail.com>
2026-09-08 16:43:50 +0530

    fix: add mongodb to package.json dependencies and serverExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  serverExternalPackages: ['jsonwebtoken', 'semver', 'mongoose', 'bcryptjs', 'razorpay', 'twilio', 'mongodb',],
+  serverExternalPackages: ['jsonwebtoken', 'semver', 'mongoose', 'bcryptjs', 'razorpay', 'twilio', 'mongodb'],
   async rewrites() {
     return [
       { source: '/auth/:path*', destination: '/api/auth/:path*' },
